c1cc08ee384c82a82c3d84d7f2d3c321e32cf0f3
kawasita <115086200+kawasita@users.noreply.github.com>
2022-11-07 11:39:17 +0000

    6th commit link_toでつまずいたから質問する

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,10 +12,27 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to 
+    redirect_to book_path(@book.id)
   end
   
   def show
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(bool_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
