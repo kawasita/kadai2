1309d0e73d1d9c907b99ced7f576b48ab8017726
kawasita <115086200+kawasita@users.noreply.github.com>
2022-12-28 06:36:51 +0000

    rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+# frozen_string_literal: true
+
+require 'rails_helper'
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -30,8 +34,6 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-#Rails.logger = Logger.new(STDOUT)
-#ActiveRecord::Base.logger = Logger.new(STDOUT)
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -60,5 +62,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-	config.include FactoryBot::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 end
